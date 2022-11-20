4cdd614010c6c159d09217f166b63b3b04cddbf9
hyunseokcheong <jhs710000@gmail.com>
2022-11-20 17:39:13 +0900

    221120

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,20 +5,31 @@
             <div class="row-sector">
                 <HeadBar msg="인기 영화1" />
                 <div id="popular">
-                    <carousel-3d 
-                    :height="400"
-                    :autoplay="true" :autoplay-timeout="3000" 
-                    :controls-visible="true" 
-                    :controls-prev-html="'&#x2039; '" :controls-next-html="'&#x203A;'"  :controls-width="10" :controls-height="30" 
+                    <carousel-3d
+                        :height="400"
+                        :autoplay="true"
+                        :autoplay-timeout="3000"
+                        :controls-visible="true"
+                        :controls-prev-html="'&#x2039; '"
+                        :controls-next-html="'&#x203A;'"
+                        :controls-width="10"
+                        :controls-height="30"
                     >
-                        <slide v-for="(popularmovie, i) in movies" :index="i" :key="i">
-                            <PopularMovie 
+                        <slide
+                            v-for="(popularmovie, i) in movies"
+                            :index="i"
+                            :key="i"
+                        >
+                            <PopularMovie
                                 :popularmovie="popularmovie"
                                 :index="i"
                             />
                         </slide>
                     </carousel-3d>
                 </div>
+                <!--  -->
+                <TestMovie />
+                <!--  -->
                 <HeadBar msg="영화 추천" />
                 <div class="cards-box">
                     <RecommendMovie
@@ -59,11 +70,12 @@
 
 <script>
 import RecommendMovie from "@/components/RecommendMovie";
-import Random from "@/components/Random"
+import Random from "@/components/Random";
 import SideBar from "@/components/SideBar";
 import HeadBar from "@/components/HeadBar";
-import { Carousel3d, Slide } from 'vue-carousel-3d';
-import PopularMovie from '@/components/PopularMovie';
+import { Carousel3d, Slide } from "vue-carousel-3d";
+import PopularMovie from "@/components/PopularMovie";
+import TestMovie from "@/components/TestMovie";
 
 export default {
     name: "HomeView",
@@ -74,7 +86,8 @@ export default {
         HeadBar,
         Carousel3d,
         Slide,
-        PopularMovie
+        PopularMovie,
+        TestMovie,
     },
     created() {
         this.getMovie();
