8b78ef2e6ed27e9936741023771ca037d09390f1
HyunseokCheong <jhs710000@gmail.com>
2022-11-21 17:41:23 +0900

    221121

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,43 +17,10 @@ export default {
 };
 </script>
 <style>
+@import "./assets/styles/app.css";
 @import "./assets/styles/navbar.css";
 @import "./assets/styles/movieslider.css";
 @import "./assets/styles/tinder.css";
-
-/* 전체 */
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    /*  */
-    z-index: 2;
-}
-/* 현재 페이지 강조 */
-nav a.router-link-exact-active {
-    color: #42b983;
-}
-.col-sector {
-    width: 100%;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-
-    border: solid 1px black;
-}
-.row-sector {
-    width: 90%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    border: solid 1px black;
-}
-.tmp {
-    height: 68px;
-}
+@import "./assets/styles/navbar.css";
+@import "./assets/styles/popup.css";
 </style>
